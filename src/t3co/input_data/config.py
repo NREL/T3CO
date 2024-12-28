e97654bb97c7b9be9535b460b1400d3f40b773d7
Harish Panneer Selvam <panne027@umn.edu>
2024-12-28 01:45:20 -0700

    Create config and scenario under input_data

---
+++ src/t3co/input_data/config.py
@@ -0,0 +1,143 @@
+import ast
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Self
+import numpy as np
+import pandas as pd
+import t3co.constants.Global as gl
+
+@dataclass
+class Config:
+    """
+    This class reads T3COConfig.csv file containing analysis attributes like vehicle and scenario paths, TCO_method, and scenario attribute overrides.
+
+    """
+
+    analysis_id: int = 0
+    analysis_name: str = ""
+    vehicle_file: str = ""
+    scenario_file: str = ""
+    dst_dir: str = ""
+    resfile_suffix: str = None
+    write_tsv: bool = False
+    selections: str = ""
+    # selections: list = field(default_factory=list)
+    vehicle_life_yr: float = 0
+    drive_cycle: str = None
+    # Fueling
+    ess_max_charging_power_kw: float = 0
+    fs_fueling_rate_kg_per_min: float = 0
+    fs_fueling_rate_gasoline_gpm: float = 0
+    fs_fueling_rate_diesel_gpm: float = 0
+
+    TCO_method: str = "DIRECT"
+
+    # Optimization
+    algorithms: str = ""
+    lw_imp_curves: str = ""
+    eng_eff_imp_curves: str = ""
+    aero_drag_imp_curves: str = ""
+    lw_imp_curve_sel: str = ""
+    eng_eff_imp_curve_sel: str = ""
+    aero_drag_imp_curve_sel: str = ""
+    skip_all_opt: bool = True
+    constraint_range: bool = False
+    constraint_accel: bool = False
+    constraint_grade: bool = False
+    objective_tco: bool = False
+    constraint_c_rate: bool = False
+    constraint_trace_miss_dist_percent_on: bool = False
+    objective_phev_minimize_fuel_use: bool = False
+
+    # Opportunity Cost
+    activate_tco_payload_cap_cost_multiplier: bool = False
+    activate_tco_fueling_dwell_time_cost: bool = False
+    fdt_frac_full_charge_bounds: list = field(default_factory=list)
+    activate_mr_downtime_cost: bool = False
+
+    def from_file(self, filename: str, analysis_id: int) -> Self:
+        """
+        This method generates a Config dictionary from CSV file and calls Config.from_dict
+
+        Args:
+            filename (str): path of input T3CO Config file
+            analysis_id (int): analysis ID selections
+
+        Returns:
+            Self.from_dict: method that gets Config instance from config_dict
+        """
+        filename = str(filename)
+
+        config_df = (
+            pd.read_csv(filename, index_col="analysis_id")
+            .loc[analysis_id]
+            .replace({np.nan: None})
+        )
+        config_dict = config_df.to_dict()
+
+        return self.from_dict(config_dict=config_dict)
+
+    def from_dict(self, config_dict: dict) -> Self:
+        """
+        This method generates a Config instance from config_dict
+
+        Args:
+            config_dict (dict): dictionary containing fields from T3CO Config input CSV file
+
+        Returns:
+            Self: Config instance containining all values from T3CO Config CSV file
+        """
+        try:
+            config_dict["selections"] = ast.literal_eval(config_dict["selections"])
+        except:  # noqa: E722
+            config_dict["selections"] = int(config_dict["selections"])
+        self.__dict__.update(config_dict)
+
+    def validate_analysis_id(self, filename: str, analysis_id: int = 0) -> Self:
+        """
+        This method validates that correct analysis id is input
+
+        Args:
+            filename (str): T3CO Config input CSV file path
+
+        Raises:
+            Exception: Error if analysis_id not found
+        """
+        filename = str(filename)
+        config_df = pd.read_csv(filename)
+        print(f"Try these analysis IDs instead: {list(config_df['analysis_id'])}")
+        assert (
+            analysis_id in config_df["analysis_id"]
+        ), "Given analysis_id not in config input file"
+        raise Exception
+
+    def check_drivecycles_and_create_selections(self, config_file: str | Path):
+        """
+        This method checks if the config.drive_cycle input is a file or a folder. If a folder is provided, then it creates a list of all selections for each drivecycle in the folders as config.dc_files
+
+        Args:
+            config_file (str|Path): File path of config file
+        """
+        self.dc_files = None
+        try:
+            if Path(self.drive_cycle).is_absolute():
+                dc_folder_path = Path(self.drive_cycle)
+            else:
+                dc_folder_path = Path(config_file).parent / self.drive_cycle
+            if not dc_folder_path.exists():
+                try:
+                    dc_folder_path = gl.OPTIMIZATION_DRIVE_CYCLES / self.drive_cycle
+                except:
+                    print(f"Drivecycle folder does not exist: {dc_folder_path}")
+
+            if Path(dc_folder_path).is_dir():
+                self.dc_files = [p.absolute() for p in dc_folder_path.rglob("*.csv")]
+                selections_list = list(self.selections)
+                self.selections = []
+                for selection in selections_list:
+                    for i in range(len(self.dc_files)):
+                        self.selections.append(str(selection) + "_" + str(i).zfill(3))
+            else:
+                self.dc_files = None
+        except:
+            Exception
---
+++ src/t3co/input_data/scenario.py
@@ -0,0 +1,212 @@
+from dataclasses import dataclass, field
+
+from t3co.input_data.config import Config
+
+
+@dataclass
+class Scenario:
+    """
+    Class object that contains all TCO parameters and performance target (range, grade, accel) information \
+        for a vehicle such that performance and TCO can be computed during optimization
+    """
+
+    selection: float = 0
+    drive_cycle: str = ""
+    use_config: bool = True
+    vmt_reduct_per_yr: float = 0
+    vmt: list = field(default_factory=list)
+    constant_trip_distance_mi: float = 0
+    vehicle_life_yr: float = 0
+    desired_ess_replacements: float = 0
+    discount_rate_pct_per_yr: float = 0
+
+    ess_max_charging_power_kw: float = 0
+    ess_cost_dol_per_kw: float = 0
+    ess_cost_dol_per_kwh: float = 0
+    ess_base_cost_dol: float = 0
+    ess_cost_reduction_dol_per_yr: float = 0
+    ess_salvage_value_dol: float = 0
+    ess_charge_rate_kW: float = 0
+    pe_mc_cost_dol_per_kw: float = 0
+    pe_mc_base_cost_dol: float = 0
+    fc_ice_cost_dol_per_kw: float = 0
+    fc_ice_base_cost_dol: float = 0
+    fc_fuelcell_cost_dol_per_kw: float = 0
+    fs_cost_dol_per_kwh: float = 0
+    fs_h2_cost_dol_per_kwh: float = 0
+    plug_base_cost_dol: float = 0
+    markup_pct: float = 0
+    tax_rate_pct: float = 0
+    fc_cng_ice_cost_dol_per_kw: float = 0
+    fs_cng_cost_dol_per_kwh: float = 0
+    vehicle_glider_cost_dol: float = 0
+    segment_name: str = ""
+    gvwr_kg: float = 0
+    gvwr_credit_kg: float = 0
+    # a list of fuels, basecase fuel is singleton list
+    fuel_type: list = field(default_factory=list)
+    maint_oper_cost_dol_per_mi: list = field(default_factory=list)
+    vocation: str = ""
+    vehicle_class: str = ""
+    model_year: float = 0
+    region: str = ""
+    target_range_mi: float = 0
+    min_speed_at_6pct_grade_in_5min_mph: float = 0
+    min_speed_at_1p25pct_grade_in_5min_mph: float = 0
+    max_time_0_to_60mph_at_gvwr_s: float = 0
+    max_time_0_to_30mph_at_gvwr_s: float = 0
+    # TDA vars
+    lw_imp_curve_sel: str = ""
+    eng_eff_imp_curve_sel: str = ""
+    aero_drag_imp_curve_sel: str = ""
+    # computed vars
+    # scenario_gge_regional_temporal_fuel_price: str = ""
+    originalcargo_kg: float = (
+        -1.0
+    )  # if needed, should be assigned immediately after vehicle read in
+    # For adding mass from CdA during optimization. veh_kg = glider_kg + powertrainKg, where
+    # glider_kg is assigned the value of originalglider_kg + CdAKg
+    originalglider_kg: float = -1.0
+    # for adding incremental cost to glider from different CdA guesses in moo loop
+    originalGliderPrice: float = -1.0
+    # for adding percent improvemnt cost to engine efficiency when optimizing CONV
+    originalIceDolPerKw: float = -1.0
+    # for adjusting fuel converter efficiency based on new peak eff
+    origfc_eff_map: list = field(default_factory=list)
+    # for adjusting drag coefficient of vehicle
+    originaldrag_coef: float = -1
+
+    ess_init_soc_grade: float = -1.0
+    ess_init_soc_accel: float = -1.0
+
+    soc_norm_init_for_accel_pct: float = -1
+    soc_norm_init_for_grade_pct: float = -1
+
+    # fuel storage
+    fs_fueling_rate_gasoline_gpm: float = 0
+    fs_fueling_rate_diesel_gpm: float = 0
+    fs_fueling_rate_kg_per_min: float = 0
+
+    ### PHEV stuff
+    # UF for % of miles in charge depleting mode
+    phev_utility_factor_override: float = -1
+    phev_utility_factor_computed: float = -1
+    # percent (fractional) of motor power for setting kw_fc_demand_on during optimization
+    motor_power_override_kw_fc_demand_on_pct: float = -1
+
+    # This will be used to figure out the number of miles travelled before needing to charge
+    # must be greater than 0
+    shifts_per_year: list = field(default_factory=list)
+
+    missed_trace_correction: bool = False
+    max_time_dilation: float = -1
+    min_time_dilation: float = -1
+    time_dilation_tol: float = -1
+
+    #
+    ### Optimization Settings
+    #
+    skip_opt: bool = False
+    knob_min_ess_kwh: list = field(default_factory=list)
+    knob_max_ess_kwh: list = field(default_factory=list)
+    knob_min_motor_kw: list = field(default_factory=list)
+    knob_max_motor_kw: list = field(default_factory=list)
+    knob_min_fc_kw: list = field(default_factory=list)
+    knob_max_fc_kw: list = field(default_factory=list)
+    knob_min_fs_kwh: list = field(default_factory=list)
+    knob_max_fs_kwh: list = field(default_factory=list)
+    # placeholder for if max_c_rate need to be entered as parameters for each scenario
+    # c_rate_kwh_array: list = field(default_factory=list)
+    # c_rate_array: list = field(default_factory=list)
+    objective_phev_minimize_fuel_use: bool = False
+    constraint_c_rate: bool = False
+    constraint_range: bool = False
+    constraint_accel: bool = False
+    constraint_grade: bool = False
+    objective_tco: bool = False
+    constraint_trace_miss_dist_percent_on: bool = False
+    trace_miss_dist_percent: float = 0
+    constraint_phev_minimize_fuel_use_on: bool = False
+    constraint_phev_minimize_fuel_use_percent: float = 0
+
+    #
+    ### TCO Element Activations and vars
+    #
+    labor_rate_dol_per_hr: float = 0
+    downtime_oppy_cost_dol_per_hr: float = 0
+
+    # payload loss factor vars, PLF
+    activate_tco_payload_cap_cost_multiplier: bool = True
+    plf_ref_veh_empty_mass_kg: float = 0
+    plf_scenario_vehicle_empty_kg: float = 0
+    plf_reference_vehicle_cargo_capacity_kg: float = 0
+    plf_scenario_vehicle_cargo_capacity_kg: float = 0  # includes cargo credit kg
+    estimated_lost_payload_kg: float = 0
+
+    # Fueling Dwell time factors, FDT
+    activate_tco_fueling_dwell_time_cost: bool = False
+    dlf_min_charge_time_hr: float = 0
+    fdt_dwpt_fraction_power_pct: float = 0
+    fdt_avg_overhead_hr_per_dwell_hr: float = 0
+    fdt_frac_full_charge_bounds: float = 0
+    fdt_num_free_dwell_trips: float = 0
+    fdt_available_freetime_hr: float = 0
+    # Insurance factors
+    insurance_rates_pct_per_yr: list = field(default_factory=list)
+
+    # Residual Rate
+    residual_rate_pct: float = 0
+
+    # Maintenance and Repair Downtime factors MR
+    activate_mr_downtime_cost: bool = False
+    mr_planned_downtime_hr_per_yr: float = 0
+    mr_unplanned_downtime_hr_per_mi: list = field(default_factory=list)
+    mr_avg_tire_life_mi: float = 0
+    mr_tire_replace_downtime_hr_per_event: float = 0
+
+    def from_config(self, config: Config = None, verbose: bool = False) -> None:
+        """
+        This method overrides certain scenario fields if use_config is True and config object is not None
+
+        Args:
+            config (Config, optional): Config object. Defaults to None.
+
+        """
+        fields_override = [
+            "vehicle_life_yr",
+            "fs_fueling_rate_kg_per_min",
+            "fs_fueling_rate_gasoline_gpm",
+            "fs_fueling_rate_diesel_gpm",
+            "lw_imp_curve_sel",
+            "eng_eff_imp_curve_sel",
+            "aero_drag_imp_curve_sel",
+            "constraint_range",
+            "constraint_accel",
+            "constraint_grade",
+            "objective_tco",
+            "constraint_c_rate",
+            "constraint_trace_miss_dist_percent_on",
+            "objective_phev_minimize_fuel_use",
+            "activate_tco_payload_cap_cost_multiplier",
+            "activate_tco_fueling_dwell_time_cost",
+            "fdt_frac_full_charge_bounds",
+            "activate_mr_downtime_cost",
+        ]
+        if config.dc_files == None:
+            fields_override.append("drive_cycle")
+        self.fields_overriden = []
+        if self.use_config == True and config != None:
+            for field_select in fields_override:
+                if config.__dict__[field_select] != None:
+                    # and (
+                    # not self.__dict__[field_select])
+                    setattr(self, field_select, config.__getattribute__(field_select))
+                    # print(f'field: {field}, type: {type(self.__getattribute__(field))}, value: {self.__getattribute__(field)}')
+                    self.fields_overriden.append(field_select)
+            print(
+                f"Scenario Fields overridden from config: {self.fields_overriden}"
+            ) if verbose else None
+        else:
+            print(
+                f"Config file not attached or scenario.use_config set to False: {config}"
+            )
