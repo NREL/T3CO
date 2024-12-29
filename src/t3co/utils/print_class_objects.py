78dd0a985fc1339dada5040269ad55fdc8250031
Harish Panneer Selvam <panne027@umn.edu>
2024-12-29 15:02:06 -0700

    Modify obj_to_string to print lists of instances

--- src/t3co/utils/print_class_objects.py
+++ src/t3co/utils/print_class_objects.py
@@ -1,6 +1,19 @@
+
+from typing import List
+
+
 def obj_to_string(obj, extra='    '):
-    return str(obj.__class__) + '\n' + '\n'.join(
+    if isinstance(obj, list) or isinstance(obj, List):  # Check if the object is a list
+        print(obj)
+        return '[\n' + ',\n'.join(
+            extra + obj_to_string(item, extra + '    ') if hasattr(item, '__dict__') else extra + str(item)
+            for item in obj
+        ) + '\n' + extra[:-4] + ']'  # Match the indentation level
+    
+    elif hasattr(obj, '__dict__'):  # Check if the object has __dict__ attribute
+        return str(obj.__class__) + '\n' + '\n'.join(
         (extra + (str(item) + ' = ' +
-                  (obj_to_string(obj.__dict__[item], extra + '    ') if hasattr(obj.__dict__[item], '__dict__') else str(
-                      obj.__dict__[item])))
-         for item in sorted(obj.__dict__)))
+                  obj_to_string(obj.__dict__[item], extra + '    ') ))
+                  for item in sorted(obj.__dict__))
+    else:  # For other data types
+        return str(obj)
