7cddb592d9c1462e2d9bd5ab6afbe4f147de4aa1
Harish Panneer Selvam <panne027@umn.edu>
2024-12-29 13:15:57 -0700

    Create TCOCalc submodule

---
+++ src/t3co/tco/tcocalc.py
@@ -0,0 +1,31 @@
+from t3co.cost_models.capital_costs import CapitalCosts
+from t3co.cost_models.operating_costs import OperatingCosts
+from t3co.cost_models.opportunity_costs import OpportunityCosts
+from t3co.energy_models.energy import Energy
+from t3co.input_data.config import Config
+from t3co.input_data.scenario import Scenario
+from t3co.input_data.vehicle import Vehicle
+from t3co.utils.print_class_objects import obj_to_string
+
+class TCOCalc():
+    year_index: int = None
+    cap_costs_dol: CapitalCosts = None
+    oper_costs_dol: OperatingCosts = None
+    oppy_costs_dol: CapitalCosts = None
+
+    def __init__(self, year_index: int, vehicle:Vehicle, scenario: Scenario, energy: Energy):
+        self.calculate_capital_costs(vehicle=vehicle, scenario=scenario)
+        self.calculate_operating_costs(year_number=year_index, vehicle=vehicle, scenario=scenario,energy=energy)
+        self.calculate_opportunity_costs(year_number=year_index, vehicle=vehicle, scenario=scenario, energy=energy)
+    
+    def calculate_capital_costs(self, vehicle: Vehicle, scenario: Scenario):
+        self.cap_costs_dol = CapitalCosts(vehicle=vehicle, scenario=scenario)
+
+    def calculate_operating_costs(self, year_number, vehicle: Vehicle, scenario: Scenario, energy: Energy):
+        self.oper_costs_dol =  OperatingCosts(year_number=year_number, cap_costs=self.cap_costs_dol, vehicle=vehicle, scenario=scenario, energy=energy)
+    
+    def calculate_opportunity_costs(self, year_number:int , vehicle: Vehicle, scenario: Scenario, energy: Energy):
+        self.oppy_costs_dol =  OpportunityCosts(year_number=year_number, vehicle=vehicle, scenario=scenario, energy=energy)
+
+    def __str__(self):
+        return obj_to_string(self)
