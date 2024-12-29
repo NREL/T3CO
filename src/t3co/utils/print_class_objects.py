c6bad427e0138a2988489341e6ed9ccf1ac2a314
Harish Panneer Selvam <panne027@umn.edu>
2024-12-29 13:15:39 -0700

    Add print_class_objects to utils

---
+++ src/t3co/utils/print_class_objects.py
@@ -0,0 +1,6 @@
+def obj_to_string(obj, extra='    '):
+    return str(obj.__class__) + '\n' + '\n'.join(
+        (extra + (str(item) + ' = ' +
+                  (obj_to_string(obj.__dict__[item], extra + '    ') if hasattr(obj.__dict__[item], '__dict__') else str(
+                      obj.__dict__[item])))
+         for item in sorted(obj.__dict__)))
