4dec84242eef6c62f0489669c507f3721a016264
Harish Panneer Selvam <panne027@umn.edu>
2026-01-05 13:12:41 -0800

    fix: fix energy with exogenous values

--- src/t3co/energy_models/energy.py
+++ src/t3co/energy_models/energy.py
@@ -46,9 +46,8 @@ class Energy:
             mpgge (float, optional): Miles per gallon gasoline equivalent. Defaults to None.
             primary_fuel_range_mi (float, optional): Primary fuel range in miles. Defaults to None.
         """
-        if mpgge is not None:
+        if mpgge is not None and primary_fuel_range_mi is not None:
             self.mpgge = mpgge
-        if primary_fuel_range_mi is not None:
             self.primary_fuel_range_mi = primary_fuel_range_mi
 
     def run_fastsim_model(
